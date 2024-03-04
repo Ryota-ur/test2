7086e162f739796fa508e9bea61a66d0d50f467b
Ryota.u <142698634+Ryota-ur@users.noreply.github.com>
2024-03-05 08:39:06 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 
+const btn = document.querySelector('.drawer-btn');
+const overlay = document.querySelector('.over-lay');
+const sideBlock = document.querySelector('.menu__sideBlock');
+
+btn.addEventListener('click',()=>{
+btn.classList.toggle('open');
+overlay.classList.toggle('hide');
+sideBlock.classList.toggle('slide');
+});
+
+overlay.addEventListener('click',()=>{
+overlay.classList.remove('hide');
+btn.classList.remove('open');
+sideBlock.classList.remove('slide');
+});
+
